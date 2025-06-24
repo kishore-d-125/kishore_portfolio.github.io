38f2022a9859a29e1f29628f7048e586b467d02f
Kishore Dharmarajan <kishored989@gmail.com>
2025-06-24 18:22:23 +0530

    Fix custom domain configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kishore_portfolio.github.io/",
+  base: "/",
   server: {
     host: true,
     port: 8080,
